0536d258e41bf9ed337e1885ed10671df69b8945
Prithiviraj R <prithiviraj.iitm@gmail.com>
2022-07-01 23:42:29 +0530

    added comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,3 +1,4 @@
+#checks for unassigned positions in the grid and returns False if grid is filled/solved
 def is_empty(grid, pos):
     for c_row in range(9):
         for c_col in range(9):
@@ -7,18 +8,21 @@ def is_empty(grid, pos):
                 return True
     return False
 
+#checks if the number to be assigned is already present in that row and returns false if it is present
 def row_safe(grid, row, num):
     for i in range(9):
         if grid[row][i] == num:
              return False
     return True
 
+#checks if the number to be assigned is already present in that colum and returns false if it is present
 def col_safe(grid, col, num):
     for i in range(9):
         if grid[i][col] == num:
             return False
     return True
 
+#checks if the number to be assigned is already present in that box and returns false if it is present
 def box_safe(grid,row,col,num):
     box_row = row - row % 3
     box_col = col - col % 3
@@ -29,11 +33,13 @@ def box_safe(grid,row,col,num):
                 return False
     return True
 
+#check if it is safe to assign the number in that position
 def is_safe(grid,row,col,num):
      return row_safe(grid, row, num) and col_safe(grid, col, num) and box_safe(grid, row, col, num)
 
+#solves the sudoku using backtracking
 def solve(grid):
-    pos = [0,0]
+    pos = [0,0]  
 
     if not is_empty(grid,pos):
         return True
@@ -46,9 +52,11 @@ def solve(grid):
             grid[row][col] = num
             if solve(grid):
                 return True
-            grid[row][col] = 0
+            grid[row][col] = 0         #backtracking
 
     return False
+
+
 
 
 
